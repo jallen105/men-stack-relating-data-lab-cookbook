84a8c91bb3f3662bb1ab2e2670597e47c49ee05a
Jonathan <jonathanallen105@gmail.com>
2025-09-05 12:25:48 -0400

    added a delete route and option in the index

--- controllers/foods.js
+++ controllers/foods.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const User = require('../models/user.js')
 
 router.get('/', async (req, res) => {
-    
+
     try {
             const currentUser = await User.findById(req.session.user._id)
         res.render('foods/index.ejs', {
@@ -21,7 +21,7 @@ router.get('/new', (req, res) => {
     res.render('foods/new.ejs')
 })
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
 
     try {
         const currentUser = await User.findById(req.session.user._id)
@@ -33,6 +33,18 @@ router.post('/', async(req, res) => {
         res.redirect('/')
     }
     
+})
+
+router.delete('/:itemId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        currentUser.pantry.id(req.params.itemId).deleteOne()
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/foods`)
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
 })
 
 module.exports = router
